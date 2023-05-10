e2f705f0ecad485ea94395220e89d15dfb651df8
septmustadmin <133139967+septmustadmin@users.noreply.github.com>
2023-05-11 03:05:30 +0800

    Update settings.py

--- user_management/settings.py
+++ user_management/settings.py
@@ -135,7 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'    
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
