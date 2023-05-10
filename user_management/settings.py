194f93631c6cadcc09b09997d4f812b806fdebfa
septmustadmin <133139967+septmustadmin@users.noreply.github.com>
2023-05-11 03:19:29 +0800

    Update settings.py

--- user_management/settings.py
+++ user_management/settings.py
@@ -135,13 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'    
+STATIC_ROOT = BASE_DIR / os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+STATIC_URL = '/static/'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
